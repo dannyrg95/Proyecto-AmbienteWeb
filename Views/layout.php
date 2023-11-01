81451d759d98c2d1dc98dfcce00b17f9ed1ce9a6
Joel <joel33960gmail.com>
2023-10-31 21:06:41 -0600

    header index.php

---
+++ Views/layout.php
@@ -0,0 +1,33 @@
+<?php 
+function MostrarHeader()
+{
+    echo
+    '    <!-- Navbar Start -->
+    <header class="">
+            <nav class="">
+            <ul class="">
+                    <li>
+                        <a href="" class="">Project Management</a>
+                    </li>
+                    <li>
+                        <a href="#" class="">Inicio</a>
+                    </li>
+                    <li>
+                        <a href="#" class="">Tareas</a>                    
+                    </li>
+                    <li>
+                        <a href="#" class="">Proyectos</a>  
+                    </li>
+                    <li>
+                        <a href="#" class="">Empleados</a>  
+                    </li>
+                    
+                    
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <!-- Navbar End -->';
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    include_once("Views/layout.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="styles/main.css" rel="stylesheet">
+    <title>Proyecto</title>
+</head>
+<body>
+    <?php
+        MostrarHeader();
+    ?>
+</body>
+</html>
