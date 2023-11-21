ff704cf067b1895f3d4d9b7ce7c5f903683be344
Joel <joel33960gmail.com>
2023-11-20 23:57:49 -0600

    correcciones en los scripts de SQL

--- proyectoAmbienteWeb.sql
+++ proyectoAmbienteWeb.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE proyecto_ambiente;
 
-USE proeycto_ambiente;
+USE proyecto_ambiente;
 CREATE TABLE Proyectos (
 	id_proyecto INT PRIMARY KEY AUTO_INCREMENT,
     nombre VARCHAR(30),
@@ -18,7 +18,7 @@ CREATE TABLE Empleados (
     nombre VARCHAR(30),
     apellidos VARCHAR(40),
 	activo BOOLEAN,
-    fecha_registro DATE DEFAULT now()
+    fecha_registro DATETIME DEFAULT now()
 )
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4;
@@ -41,7 +41,7 @@ CREATE TABLE Tareas (
     titulo VARCHAR(30),
     horas VARCHAR(30),
     id_empleado INT,
-    FOREIGN KEY (id_empleado) REFERENCES Empleado(id_empleado)
+    FOREIGN KEY (id_empleado) REFERENCES Empleados(id_empleado)
 )
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4;
@@ -69,5 +69,48 @@ DEFAULT CHARACTER SET = utf8mb4;
 
 
 CREATE USER 'proyecto_ambiente'@'localhost' IDENTIFIED BY '123456';
-GRANT ALL PRIVILEGES ON *.* TO 'proeycto_ambiente'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'proyecto_ambiente'@'localhost';
 FLUSH PRIVILEGES;
+
+
+
+
+-- Insert 1
+INSERT INTO Empleados (correo, nombre, apellidos, activo) 
+VALUES ('empleado1@example.com', 'Juan', 'Perez', true);
+
+-- Insert 2
+INSERT INTO Empleados (correo, nombre, apellidos, activo) 
+VALUES ('empleado2@example.com', 'Maria', 'Gomez', true);
+
+-- Insert 3
+INSERT INTO Empleados (correo, nombre, apellidos, activo) 
+VALUES ('empleado3@example.com', 'Carlos', 'Lopez', false);
+
+-- Insert 4
+INSERT INTO Empleados (correo, nombre, apellidos, activo) 
+VALUES ('empleado4@example.com', 'Laura', 'Rodriguez', true);
+
+-- Insert 5
+INSERT INTO Empleados (correo, nombre, apellidos, activo) 
+VALUES ('empleado5@example.com', 'Pedro', 'Martinez', false);
+
+-- Insert 6
+INSERT INTO Empleados (correo, nombre, apellidos, activo) 
+VALUES ('empleado6@example.com', 'Ana', 'Sanchez', true);
+
+-- Insert 7
+INSERT INTO Empleados (correo, nombre, apellidos, activo) 
+VALUES ('empleado7@example.com', 'Diego', 'Gutierrez', true);
+
+-- Insert 8
+INSERT INTO Empleados (correo, nombre, apellidos, activo) 
+VALUES ('empleado8@example.com', 'Elena', 'Fernandez', false);
+
+-- Insert 9
+INSERT INTO Empleados (correo, nombre, apellidos, activo) 
+VALUES ('empleado9@example.com', 'Sergio', 'Hernandez', true);
+
+-- Insert 10
+INSERT INTO Empleados (correo, nombre, apellidos, activo) 
+VALUES ('empleado10@example.com', 'Carmen', 'Diaz', true);
